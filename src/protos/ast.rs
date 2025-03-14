730875e4239195be8f0881eb5591815d179b34c2
zukkhs <zukkhs@gmail.com>
2025-03-14 12:56:09 +0100

    feat(parser): :sparkles: add macros shortcut for ast building

--- src/protos/ast.rs
+++ src/protos/ast.rs
@@ -9,3 +9,17 @@ pub enum Expression {
     T(Term),
     R(Box<Relation>)
 }
+
+#[macro_export]
+macro_rules! t {
+    ($t:tt) => {
+        $crate::protos::ast::Expression::T($crate::protos::ast::Term($t.to_string()))
+    };
+}
+
+#[macro_export]
+macro_rules! r {
+    ($a:expr, $b:expr) => {
+        $crate::protos::ast::Expression::R(Box::new($crate::protos::ast::Relation($a, $b)))
+    };
+}
